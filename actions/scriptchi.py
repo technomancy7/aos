2470ee04f1d4ebd255b8bc1cda330854fcb7d1bb
technomancy7 <kylebell68@gmail.com>
2024-06-07 13:38:25 +0100

    sync

--- actions/scriptchi.py
+++ actions/scriptchi.py
@@ -1,7 +1,7 @@
 
 import time
 
-class Scriptkiddie:
+class Scriptchi:
     def __init__(self, **d):
         self.name = d.get("name", "")
         self.hunger = d.get("hunger", 0)
@@ -23,53 +23,53 @@ class Scriptkiddie:
     def feed(self):
         self.hunger -= 1
         self.happiness += 1
-        
+
     def play(self):
         self.happiness += 2
         self.tiredness += 1
-        
+
     def sleep(self):
         self.tiredness -= 2
-        
+
     def update(self):
         if self.alive:
             self.hunger += 1
             self.tiredness += 1
             self.age += 1
-            
+
             if self.hunger >= 10 or self.tiredness >= 10:
                 self.alive = False
-                
+
             if self.happiness < 0:
                 self.happiness = 0
             elif self.happiness > 10:
                 self.happiness = 10
-                
+
             if self.tiredness < 0:
                 self.tiredness = 0
             elif self.tiredness > 10:
                 self.tiredness = 10
-            
+
             if self.hunger < 0:
                 self.hunger = 0
             elif self.hunger > 10:
                 self.hunger = 10
-    
+
     def is_alive(self):
         return self.alive
-    
+
     def get_hunger(self):
         return self.hunger
-    
+
     def get_happiness(self):
         return self.happiness
-    
+
     def get_tiredness(self):
         return self.tiredness
-    
+
     def get_age(self):
         return self.age
-    
+
     def __str__(self):
         if self.alive:
             return f"{self.name} (HU: {self.hunger}  HA: {self.happiness}  TI: {self.tiredness})"
@@ -81,7 +81,7 @@ class Action:
     @staticmethod
     def __action__():
         return {
-            "name": "scriptkiddie",
+            "name": "scriptchi",
             "author": "Kai",
             "version": "0.0",
             "features": [],
@@ -93,16 +93,16 @@ class Action:
             Default help.
         """
 
-    def __run__(self, ctx): 
-        tg = Scriptkiddie(name = "Testy")
+    def __run__(self, ctx):
+        tg = Scriptchi(name = "Testy")
 
         print(tg.to_dict())
         #tg.rest()
         tg.update()
         print(tg.to_dict())
         clone = tg.to_dict()
-        clone['name'] = "Clonekiddie"
-        ctg = Scriptkiddie(**clone)
+        clone['name'] = "Clone"
+        ctg = Scriptchi(**clone)
         print(ctg, ctg.to_dict())
         # Main functionality here.
         return ctx
@@ -112,4 +112,3 @@ class Action:
 
     def __finish__(self, ctx):
         pass
-
--- actions/wiki.py
+++ actions/wiki.py
@@ -17,53 +17,54 @@ class Action:
 
     def __help__(self, ctx):
         return """\
-        Commands:
+        Shows summary of search term.
 
-            open <search>
-                Opens search in default webbrowser
-
-            summary <search>
-                Gets summary of search term.
-                Flags:
-                    -s -> Simple output mode.\
+        Flags:
+            --browser : opens in web browser
+            --simple : only shows result
         """
 
     def __run__(self, ctx):
-        cmd, ln = ctx.cmdsplit()
-        simple_mode = ctx.has_flag("s")
+        ln = ctx.get_string()
+        simple_mode = ctx.has_flag("simple")
+        open_in_browser = ctx.has_flag("browser")
 
-        match cmd:
-            case "open" | "o":
-                ln = quote(ln)
-                base = "https://en.wikipedia.org/"
-                r = requests.get(f"{base}w/api.php?action=opensearch&search={ln}&limit=1&format=json").json()
-                if len(r[3]) > 0:
-                    addr = r[3][0]
-                    webbrowser.open(addr)
-                else:
-                    ctx.writeln("No results.")
-                    
-            case "summary" | "s":
-                if ln:
-                    if not simple_mode: ctx.writeln(f":right_arrow: Searching for: {ln}")
-                    ln = quote(ln)
-                    base = "https://en.wikipedia.org/"
-                    r = requests.get(f"{base}w/api.php?action=opensearch&search={ln}&limit=1&format=json").json()
-                    if len(r[3]) > 0:
-                        addr = r[3][0]
-                        if not simple_mode: ctx.writeln(f":right_arrow: Got base address: {addr}")
-                        apiaddr = addr.replace("/wiki/", "/api/rest_v1/page/summary/")
-                        if not simple_mode: ctx.writeln(f":right_arrow: Converted to API address: {apiaddr}")
+        if open_in_browser:
+            ln = quote(ln)
+            base = "https://en.wikipedia.org/"
+            if not simple_mode: ctx.writeln(f":right_arrow: Finding relevant pages...")
+            r = requests.get(f"{base}w/api.php?action=opensearch&search={ln}&limit=1&format=json").json()
 
-                        fresult = requests.get(apiaddr).json()
+            if len(r[3]) > 0:
+                addr = r[3][0]
+                name = webbrowser.get().name
+                if not simple_mode: ctx.writeln(f":right_arrow: Opening in browser {name}: {addr}")
+                webbrowser.open(addr)
+            else:
+                ctx.writeln("No results.")
+            return
 
-                        if not simple_mode: ctx.write_panel(f""" < {fresult['title']} ({fresult['description']}) >\n[blue]{fresult['extract']}""")
+        if ln:
+            if not simple_mode: ctx.writeln(f":right_arrow: Searching for: {ln}...")
+            ln = quote(ln)
+            base = "https://en.wikipedia.org/"
+            r = requests.get(f"{base}w/api.php?action=opensearch&search={ln}&limit=1&format=json").json()
 
-                        if simple_mode: ctx.writeln(fresult['extract'])
-                    else:
-                        ctx.writeln("No results.")
-                else:
-                    ctx.writeln("Summarize what?")
+            if len(r[3]) > 0:
+                addr = r[3][0]
+                if not simple_mode: ctx.writeln(f":right_arrow: Got base address: {addr}")
+                apiaddr = addr.replace("/wiki/", "/api/rest_v1/page/summary/")
+                if not simple_mode: ctx.writeln(f":right_arrow: Converted to API address: {apiaddr}")
+
+                fresult = requests.get(apiaddr).json()
+
+                if not simple_mode: ctx.write_panel(f""" < {fresult['title']} ({fresult['description']}) >\n[blue]{fresult['extract']}""")
+
+                if simple_mode: ctx.writeln(fresult['extract'])
+            else:
+                ctx.writeln("No results.")
+        else:
+            ctx.writeln("Summarize what?")
         return ctx
 
     def __error__(self, ctx, error):
